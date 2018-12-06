45feb4e2afeff5bd8f6c0f5dd42fba67c2702ffa
Student <example@example.com>
2018-12-05 16:07:48 -0800

    Create help queue with firebase

---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA8tizFplSxVIKAqGTozXH_Nuecf-F1pJU',
+  authDomain: 'help-queue-2.firebaseapp.com',
+  databaseURL: 'https://help-queue-2.firebaseio.com',
+  projectId: 'help-queue-2',
+  storageBucket: 'help-queue-2.appspot.com',
+  messagingSenderId: '368247989329'
+};
+
+export default firebaseConfig;
